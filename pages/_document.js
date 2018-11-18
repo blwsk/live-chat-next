d66b2b50dd95b027da8655767326570ee1e258c4
Kevin Bielawski <kbielawsk7@gmail.com>
2018-11-18 12:08:23 -0500

    revert bg back to #fbfbfb (#3)

--- pages/_document.js
+++ pages/_document.js
@@ -18,7 +18,7 @@ export default class MyDocument extends Document {
           <style>{`
             body,html {
               font-family: -apple-system, BlinkMacSystemFont, sans-serif;
-              background-color: cornflowerblue;
+              background-color: #fbfbfb;
             }
 
             #hs-eu-cookie-confirmation {
