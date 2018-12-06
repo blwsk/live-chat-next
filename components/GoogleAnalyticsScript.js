f0830a1ca49bc88ee076b0801cb5f7d06310d51a
Kevin Bielawski <kbielawsk7@gmail.com>
2018-12-06 14:44:48 -0500

    add analytics (#5)

---
+++ components/GoogleAnalyticsScript.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default () => (
+  <React.Fragment>
+    <script async={true} src="https://www.googletagmanager.com/gtag/js?id=UA-46337232-2" />
+    <script type="text/javascript" async={true} src="./static/analytics.js" />
+  </React.Fragment>
+);
