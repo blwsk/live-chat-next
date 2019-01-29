9bd1327cf46b57ebce7699fb40952eeabd6d4319
Kevin Bielawski <kbielawsk7@gmail.com>
2019-01-29 16:28:35 -0500

    add inline embed on new splash page (#6)

--- pages/pricing.js
+++ pages/pricing.js
@@ -1,8 +1,24 @@
+import React, { Component } from 'react';
 import Footer from '../components/Footer';
+import { safelyLoadLiveChat } from '../utils/safelyLoadLiveChat';
 
-export default () => (
-  <div>
-    <h1>Pricing</h1>
-    <Footer />
-  </div>
-);
+class Pricing extends Component {
+  componentDidMount() {
+    window.hsConversationsSettings = {
+      loadImmediately: false
+    };
+
+    safelyLoadLiveChat();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Pricing</h1>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Pricing;
