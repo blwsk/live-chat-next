89d5146e37403a639ce189e836101c8ac7c25e45
Kevin Bielawski <kbielawski@hubspot.com>
2019-01-30 18:24:23 -0500

    add sidebar to splash page

--- pages/splash.js
+++ pages/splash.js
@@ -4,6 +4,14 @@ import { safelyLoadLiveChat } from '../utils/safelyLoadLiveChat';
 const INLINE_EMBED_ID = 'live-chat-inline-embed';
 
 class Splash extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sidebarOpen: false
+    };
+  }
+
   componentDidMount() {
     if (
       window.hsConversationsSettings &&
@@ -12,16 +20,51 @@ class Splash extends Component {
       window.hsConversationsSettings = Object.assign({}, window.hsConversationsSettings, {
         inlineEmbedSelector: `#${INLINE_EMBED_ID}`
       });
+    }
+  }
 
+  componentDidUpdate() {
+    if (this.state.sidebarOpen) {
       safelyLoadLiveChat();
     }
   }
+
+  openSidebar = () => {
+    this.setState({
+      sidebarOpen: true
+    });
+  };
+
+  closeSidebar = () => {
+    this.setState({
+      sidebarOpen: false
+    });
+  };
 
   render() {
     return (
       <div>
         <h1>Splash</h1>
-        <div id={INLINE_EMBED_ID} />
+        <button onClick={this.openSidebar}>I need help</button>
+        {this.state.sidebarOpen && (
+          <div
+            className="sidebar"
+            style={{
+              position: 'absolute',
+              right: 0,
+              top: 0,
+              bottom: 0,
+              height: '100%',
+              width: 400,
+              background: 'whitesmoke',
+              borderLeft: '1px solid lightgray',
+              padding: 12
+            }}
+          >
+            <button onClick={this.closeSidebar}>Ok...I'm good</button>
+            <div id={INLINE_EMBED_ID} />
+          </div>
+        )}
       </div>
     );
   }
