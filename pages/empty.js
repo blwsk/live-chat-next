898070b7f41c313208b99e947a78d76eb9b6247e
Kevin Bielawski <kbielawsk7@gmail.com>
2019-01-30 11:34:30 -0500

    Revert "add inline embed on new splash page (#6)" (#7)
    This reverts commit 9bd1327cf46b57ebce7699fb40952eeabd6d4319.

--- pages/empty.js
+++ pages/empty.js
@@ -1,24 +1,5 @@
-import React, { Component } from 'react';
-import Footer from '../components/Footer';
-import { safelyLoadLiveChat } from '../utils/safelyLoadLiveChat';
-
-class Empty extends Component {
-  componentDidMount() {
-    window.hsConversationsSettings = {
-      loadImmediately: false
-    };
-
-    safelyLoadLiveChat();
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Empty</h1>
-        <Footer />
-      </div>
-    );
-  }
-}
-
-export default Empty;
+export default () => (
+  <div>
+    <h1>Empty</h1>
+  </div>
+);
--- pages/gdpr.js
+++ pages/gdpr.js
@@ -1,24 +1,8 @@
-import React, { Component } from 'react';
 import Footer from '../components/Footer';
-import { safelyLoadLiveChat } from '../utils/safelyLoadLiveChat';
 
-class Gdpr extends Component {
-  componentDidMount() {
-    window.hsConversationsSettings = {
-      loadImmediately: false
-    };
-
-    safelyLoadLiveChat();
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Gdpr</h1>
-        <Footer />
-      </div>
-    );
-  }
-}
-
-export default Gdpr;
+export default () => (
+  <div>
+    <h1>Gdpr</h1>
+    <Footer />
+  </div>
+);
