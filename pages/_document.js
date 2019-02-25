beed7169bb102ca21910ed5399fe1dc9f683ab1d
Kevin Bielawski <kbielawski@hubspot.com>
2019-02-25 11:38:40 -0500

    add meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -29,6 +29,7 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <title>{"Kevin's live chat test page"}</title>
           <style>{`
             body,html {
